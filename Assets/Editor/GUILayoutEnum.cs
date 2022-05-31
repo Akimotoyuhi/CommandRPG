45d72e3e4f425d2a96c6b86075dfb989964e8cc6
AkimotoYuhi <chiisanaaki@gmail.com>
2022-05-31 10:26:48 +0900

    初期コミット

---
+++ Assets/Editor/GUILayoutEnum.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+
+public class GUILayoutEnum : EditorWindow
+{
+    public Popup op;
+    [MenuItem("Examples/Editor GUILayout Enum Popup usage")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(GUILayoutEnum));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        op = (Popup)EditorGUILayout.EnumPopup("Primitive to create:", op);
+        if (GUILayout.Button("Create"))
+            InstantiatePrimitive(op);
+    }
+
+    void InstantiatePrimitive(Popup op)
+    {
+    }
+}
---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Popup
+{
+    AAAA,
+    BBBB,
+    CCCC,
+}
+
+public class Sample : MonoBehaviour
+{
+    [SerializeField] Popup popup;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
